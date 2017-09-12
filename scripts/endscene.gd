be170e19919a60c174dd3532ef9be1931831c740
securas <ruben.so.luis@gmail.com>
2017-09-12 20:14:35 +0900

    fixed quit bug

--- scripts/endscene.gd
+++ scripts/endscene.gd
@@ -11,7 +11,9 @@ func _ready():
 		global.camera.align()
 	set_process( true )
 
-
+var isending = false
 func _process(delta):
+	if isending: return
 	if Input.is_action_pressed( "btn_fire" ):
+		isending = true
 		global.main.change_map( "res://scenes/intro.tscn" )
